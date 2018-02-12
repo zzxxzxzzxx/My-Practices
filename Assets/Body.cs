90441d4e4a698fa7dc331d45ad612b393161ca9e
zzxxzxzzxx <627012408@qq.com>
2018-02-12 10:55:22 +0800

    snake game

---
+++ Assets/Body.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour {
+    //下一个身子的引用
+    public Body next;
+    public void Move(Vector3 pos)
+    {
+        //记录当前身子的位置
+        Vector3 nextPos = transform.position;
+        //当前身子移动
+        transform.position = pos;
+        if (next != null)
+        {
+            //后面身子移动
+            next.Move(nextPos);
+        }
+    }
+}
